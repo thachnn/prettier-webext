1ef3eec1cfc3f594ed0b2eb7d0760566eb0f7274
Thach Nguyen <thachnn80@gmail.com>
2025-06-15 02:38:35 +0700

    Configure vite to build App in production mode

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const stats = () => ({
+  name: 'display-modules',
+  load: { order: 'pre', handler: (id) => console.log('\t', id) },
+});
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: './',
+  mode: 'production',
+  esbuild: { minifyWhitespace: false, minifyIdentifiers: false },
+  build: {
+    modulePreload: { polyfill: false },
+    rollupOptions: {
+      //logLevel: 'debug',
+      output: {
+        generatedCode: 'es2015',
+        interop: 'esModule',
+        manualChunks: (id) => (/\bnode_modules[\\\/]/.test(id) ? 'vendor' : null),
+      },
+    },
+    commonjsOptions: { sourceMap: false, esmExternals: true },
+  },
+  server: { port: 8080, open: false },
+  plugins: [vue(), stats()],
 });
